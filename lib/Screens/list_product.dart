88cb9f753a21c463578e21734eed38faccf42147
tissrahma10 <tissrahma1@tisss-MacBook-Air.local>
2022-09-01 10:12:30 +0100

    fifth commit

--- lib/Screens/list_product.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_application_1/Screens/menuScreen/CartModel.dart';
-import 'package:flutter_application_1/Screens/menuScreen/ProductModel.dart';
-
-var productList = <ProductModel>[];
-
--- lib/User.dart
+++
@@ -1,6 +0,0 @@
-class User {
-  String id;
-  final String name;
-  final String position;
-  User({this.id = '', required this.name, required this.position});
-}
